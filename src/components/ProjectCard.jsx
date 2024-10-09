95132ade2c75d140243f6910c8acdfbe161e0b75
João Victor Medina <joaovictor.zipp@gmail.com>
2024-10-09 16:35:59 -0300

    adding customization to the portfolio

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
+import { useTheme } from './ThemeContext'; // Importe o hook useTheme
 
 const ProjectCard = ({ image, title, description, alertComponent }) => {
+  const { isDarkTheme } = useTheme(); // Obtenha o tema atual
+
   return (
-    <div className="project-card">
+    <div className={`project-card ${!isDarkTheme ? 'light' : ''}`}>
       <img src={image} alt={title} className="project-image" />
       <h3>{title}</h3>
       <p>{description}</p>
-      
+
       {alertComponent && (
         <div className="alert-container">
           {alertComponent}
